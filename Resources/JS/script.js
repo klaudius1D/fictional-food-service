028425c69989919eddd1d2d034c817242cce90f9
Klaude Dungca <klaude.dungca@gmail.com>
2019-04-19 12:40:17 -0600

    added animations using jQueries

--- Resources/JS/script.js
+++ Resources/JS/script.js
@@ -21,5 +21,44 @@ $(document).ready(function () {
     $('.js--scroll-to-start').click(function () {
         $('html, body').animate({ scrollTop: $('.js--section-features').offset().top }, 1000);
     });
+    /*
+        $(function () {
+            $('a[href*=#]:not([href=#])').click(function () {
+                if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+                    var target = $(this.hash);
+                    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                    if (target.length) {
+                        $('html,body').animate({
+                            scrollTop: target.offset().top
+                        }, 1000);
+                        return false;
+                    }
+                }
+            });
+        });*/
 
+    /* Animations on scroll */
+    $('.js--wp-1').waypoint(function (direction) {
+        $('.js--wp-1').addClass('animated fadeIn');
+    }, {
+            offset: '50%'
+        });
+
+    $('.js--wp-2').waypoint(function (direction) {
+        $('.js--wp-2').addClass('animated fadeInUp');
+    }, {
+            offset: '50%'
+        });
+
+    $('.js--wp-3').waypoint(function (direction) {
+        $('.js--wp-3').addClass('animated fadeIn');
+    }, {
+            offset: '50%'
+        });
+
+    $('.js--wp-4').waypoint(function (direction) {
+        $('.js--wp-4').addClass('animated pulse');
+    }, {
+            offset: '50%'
+        });
 });
